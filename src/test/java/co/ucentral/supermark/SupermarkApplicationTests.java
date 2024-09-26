2143ad501e5b5b33e148cc10e3f22702d8ff107e
Yeimi <ylizarazor@ucentral.edu.co>
2024-09-26 18:41:19 -0500

    (nuevo): primer commint esctructura

---
+++ src/test/java/co/ucentral/supermark/SupermarkApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.supermark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
