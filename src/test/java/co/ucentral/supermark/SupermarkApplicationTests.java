d0c24772950791bb9e954eba7c6e671ccc9c4190
Julian steven arias sierra <jariass2@ucentral.edu.co>
2024-10-27 20:25:26 -0500

    Cambios en la pagina, visible de momento

--- src/test/java/co/ucentral/supermark/SupermarkApplicationTests.java
+++ src/test/java/co/ucentral/supermark/SupermarkApplicationTests.java
@@ -5,6 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SupermarkApplicationTests {
+	SupermarkApplicationTests() {
+
+	}
 
 	@Test
 	void contextLoads() {
