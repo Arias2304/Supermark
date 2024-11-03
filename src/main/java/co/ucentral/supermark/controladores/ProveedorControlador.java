56f5c3f4cb461fc6ae2d6a73d34b966b601d5c31
ylizzr <ylizarazor@ucentral.edu.co>
2024-11-03 18:01:09 -0500

    (Ajuste):Log en el controlador y correcion de texto html

--- src/main/java/co/ucentral/supermark/controladores/ProveedorControlador.java
+++ src/main/java/co/ucentral/supermark/controladores/ProveedorControlador.java
@@ -3,6 +3,8 @@ package co.ucentral.supermark.controladores;
 import co.ucentral.supermark.persistencia.entidades.Proveedor;
 import co.ucentral.supermark.servicios.ProveedorServicio;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,23 +14,28 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class ProveedorControlador {
 
+    private static final Logger log = LogManager.getLogger(ProveedorControlador.class);
+
     private final ProveedorServicio proveedorServicio;
 
     @GetMapping
     public String listarProveedores(Model model) {
+        log.info("Listando todos los proveedores.");
         model.addAttribute("proveedores", proveedorServicio.obtenerTodos());
         model.addAttribute("proveedor", new Proveedor());
-        return "proveedores";  //
+        return "proveedores";
     }
 
     @PostMapping("/guardar")
     public String guardarProveedor(@ModelAttribute Proveedor proveedor) {
+        log.info("Guardando proveedor con NIT: {}", proveedor.getNit());
         proveedorServicio.guardar(proveedor);
         return "redirect:/proveedores";
     }
 
     @GetMapping("/editar/{nit}")
     public String editarProveedor(@PathVariable String nit, Model model) {
+        log.info("Editando proveedor con NIT: {}", nit);
         Proveedor proveedor = proveedorServicio.buscarPorNit(nit);
         model.addAttribute("proveedor", proveedor);
         model.addAttribute("proveedores", proveedorServicio.obtenerTodos());
@@ -37,6 +44,7 @@ public class ProveedorControlador {
 
     @GetMapping("/eliminar/{nit}")
     public String eliminarProveedor(@PathVariable String nit) {
+        log.info("Eliminando proveedor con NIT: {}", nit);
         proveedorServicio.borrarPorNit(nit);
         return "redirect:/proveedores";
     }
