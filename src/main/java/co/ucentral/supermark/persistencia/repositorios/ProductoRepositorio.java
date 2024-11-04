330afe92f4b5bd28ed449807852bce95d984f0e7
Arias2304 <jariass2@ucentral.edu.co>
2024-11-03 22:57:58 -0500

    Revert "Mask"

--- src/main/java/co/ucentral/supermark/persistencia/repositorios/ProductoRepositorio.java
+++
@@ -1,7 +0,0 @@
-package co.ucentral.supermark.persistencia.repositorios;
-import co.ucentral.supermark.persistencia.entidades.Producto;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface ProductoRepositorio extends CrudRepository<Producto, Long> {
-}
