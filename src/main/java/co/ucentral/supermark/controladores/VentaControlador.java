03685a0f82a65908110c489193029d014c1defe2
ylizzr <ylizarazor@ucentral.edu.co>
2024-11-21 09:02:46 -0500

    (Ajuste):Seleccion de clientes y prudusctos

--- src/main/java/co/ucentral/supermark/controladores/VentaControlador.java
+++ src/main/java/co/ucentral/supermark/controladores/VentaControlador.java
@@ -1,19 +1,14 @@
 package co.ucentral.supermark.controladores;
-
-import co.ucentral.supermark.persistencia.entidades.Cliente;
-import co.ucentral.supermark.persistencia.entidades.Producto;
 import co.ucentral.supermark.persistencia.entidades.Venta;
 import co.ucentral.supermark.servicios.ClienteServicio;
 import co.ucentral.supermark.servicios.ServicioProducto;
 import co.ucentral.supermark.servicios.VentaServicio;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("/ventas")
@@ -28,55 +23,34 @@ public class VentaControlador {
     @GetMapping
     public String listarVentas(Model model) {
         model.addAttribute("ventas", ventaServicio.obtenerTodas());
-        return "ventas";
-    }
-    @GetMapping("/nueva")
-    public String nuevaVenta(Model model) {
         model.addAttribute("venta", new Venta());
         model.addAttribute("clientes", clienteServicio.obtenerTodos());
-        model.addAttribute("productos", ventaServicio.obtenerTodosProductos());
+        model.addAttribute("productos", servicioProducto.obtenerTodos());
         return "ventas";
     }
 
+
     @PostMapping("/guardar")
-    public String guardarVenta(@ModelAttribute Venta venta,
-                               @RequestParam("clienteId") Integer clienteId,
-                               @RequestParam("productos") List<String> productIds,
-                               RedirectAttributes redirectAttributes) {
-        Cliente cliente = clienteServicio.buscarPorId(clienteId);
-        if (cliente != null) {
-            venta.setCliente(cliente);
-        }
-        List<Producto> productosSeleccionados = ventaServicio.obtenerProductosPorIds(productIds);
-        venta.setProductos(productosSeleccionados);
-        ventaServicio.guardarVenta(venta);
-        redirectAttributes.addFlashAttribute("mensaje", "Venta registrada exitosamente");
+    public String guardarVenta(@ModelAttribute Venta venta){
+        ventaServicio.guardar(venta);
         return "redirect:/ventas";
+
     }
 
     @GetMapping("/editar/{id}")
-    public String editarVenta(@PathVariable Long id, Model model) {
+    public String editarVenta(@PathVariable int id, Model model) {
         Venta venta = ventaServicio.buscarPorId(id);
         model.addAttribute("venta", venta);
+        model.addAttribute("ventas", ventaServicio.obtenerTodas());
         model.addAttribute("clientes", clienteServicio.obtenerTodos());
-        model.addAttribute("productos", ventaServicio.obtenerTodosProductos());
+        model.addAttribute("productos", servicioProducto.obtenerTodos());
         return "ventas";
     }
-    @GetMapping("/ventas")
-    public String mostrarFormularioVenta(Model model) {
-        Venta venta = new Venta();
 
-        // Verificar si la lista de productos es null, y si lo es, inicializarla
-        if (venta.getProductos() == null) {
-            venta.setProductos(new ArrayList<>());
-        }
-
-        // Agregar otros datos necesarios
-        model.addAttribute("venta", venta);
-        model.addAttribute("clientes", clienteServicio.listarClientes());  // Lista de clientes disponibles
-        model.addAttribute("productos", servicioProducto.listarProductos());  // Lista de productos disponibles
-
-        return "ventas";  // Nombre de tu vista HTML
+    @GetMapping("/eliminar/{codigo}")
+    public String eliminarventa(@PathVariable int id) {
+        ventaServicio.eliminar(id);
+        return "redirect:/ventas";
     }
 
 }
--- src/main/java/co/ucentral/supermark/servicios/VentaServicio.java
+++ src/main/java/co/ucentral/supermark/servicios/VentaServicio.java
@@ -1,61 +1,49 @@
 package co.ucentral.supermark.servicios;
-
-import co.ucentral.supermark.persistencia.entidades.Cliente;
-import co.ucentral.supermark.persistencia.entidades.Producto;
 import co.ucentral.supermark.persistencia.entidades.Venta;
-import co.ucentral.supermark.persistencia.repositorios.ClienteRepositorio;
-import co.ucentral.supermark.persistencia.repositorios.ProductoRepositorio;
 import co.ucentral.supermark.persistencia.repositorios.VentaRepositorio;
+
 import lombok.AllArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
-@AllArgsConstructor
 @Service
+@AllArgsConstructor
 public class VentaServicio {
 
     private static final Logger log = LogManager.getLogger(VentaServicio.class);
-
     private final VentaRepositorio ventaRepositorio;
-    private final ClienteRepositorio clienteRepositorio;
-    private final ProductoRepositorio productoRepositorio;
 
     public List<Venta> obtenerTodas() {
         log.info("Obteniendo todas las ventas");
-        return ventaRepositorio.findAll();
-    }
-    public List<Producto> obtenerProductosPorIds(List<String> productIds) {
-        return (List<Producto>) productoRepositorio.findAllById(productIds);
+        return (List<Venta>) ventaRepositorio.findAll();
     }
 
 
-    public void guardarVenta(Venta venta) {
-        log.info("Guardando nueva venta");
-        venta.setFechaVenta(LocalDateTime.now());
-        venta.setTotal(calcularTotal(venta.getProductos()));
-        ventaRepositorio.save(venta);
+    public void guardar(Venta venta) {
+       log.info("Guardando venta por codigo: {}", venta.getId());
+       ventaRepositorio.save(venta);
     }
 
-    public Venta buscarPorId(Long id) {
+
+    public Venta buscarPorId(int id) {
         log.info("Buscando venta con ID: {}", id);
-        return ventaRepositorio.findById(id).orElse(null);
+        Optional<Venta> venta = ventaRepositorio.findById(id);
+        return venta.orElse(null);
+    }
+    public void eliminar(int id) {
+        try {
+            log.info("Eliminando venta con ID: {}", id);
+            ventaRepositorio.deleteById(id);
+        } catch (Exception e) {
+            log.error("Error al eliminar la venta con ID {}: {}", id, e.getMessage());
+        }
     }
 
-    public Cliente obtenerClientePorId(int clienteId) {
-        return clienteRepositorio.findById(clienteId).orElse(null);
-    }
-
-    public List<Producto> obtenerTodosProductos() {
-        return (List<Producto>) productoRepositorio.findAll();
-    }
-
-    private Double calcularTotal(List<Producto> productos) {
-        return productos.stream()
-                .mapToDouble(producto -> producto.getPrecio().doubleValue())
-                .sum();
+    public List<Venta> obtenerVentasPorCliente() {
+        return (List<Venta>) ventaRepositorio.findAll();
     }
 }
